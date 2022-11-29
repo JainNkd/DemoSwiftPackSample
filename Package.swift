65215506f3333f56604f7f5dee3d646e0a059777
Naveen <er.naveen1911@gmail.com>
2022-11-29 21:13:38 +0530

    update

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SDWebImage/SDWebImageWebPCoder.git", from: "0.3.0")
+//        .package(url: "https://github.com/SDWebImage/SDWebImageWebPCoder.git", from: "0.3.0")
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
