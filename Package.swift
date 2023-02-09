fb00beeff9d5553b0b98d612f997555e278ad9ec
Naveen <er.naveen1911@gmail.com>
2023-02-09 20:21:28 +0530

    update

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibraryDemo",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,15 +14,29 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-//        .package(url: "https://github.com/SDWebImage/SDWebImageWebPCoder.git", from: "0.3.0")
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+                   url: "https://github.com/FleksySDK/FleksySDK-iOS",
+                   exact: "4.3.3"
+         ),
+        .package(
+                    url: "https://github.com/FleksySDK/FleksyEngine-iOS",
+                    exact: "3.6.0"
+                ),
+        .package(
+                    url: "https://github.com/FleksySDK/iOS-CoreServices",
+                    exact: "1.3.2"
+                )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLibraryDemo",
-            dependencies: ["KulfyFramwork"]),
+            dependencies: [
+                .product(name: "FleksySDK", package: "FleksySDK-iOS"),
+                .product(name: "FleksyEngine", package: "FleksyEngine-iOS"),
+                .product(name: "iOSCoreServices", package: "iOS-CoreServices"),
+                "KulfyFramwork"]),
         .binaryTarget(
                     name: "KulfyFramwork",
                     path: "./build/KulfyFramwork.xcframework"
